c28ee057da5bceab77d091b7b1fc7d4b999e6303
dmytro.hreshchik <dmytro.hreshchik@gmail.com>
2018-04-16 22:38:35 +0300

    HW_05 homework 16.04 22:38

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,31 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query
+    void delete(Meal entity);
+
+
+    @Override
+    Optional<Meal> findById(Integer id);
+
+    @Override
+    @Transactional
+    Meal save(Meal meal);
+
+    @Override
+    List<Meal> findAll(Sort sort);
+
 }

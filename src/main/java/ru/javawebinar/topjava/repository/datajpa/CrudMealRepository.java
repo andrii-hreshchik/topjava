a3f9f227d46445a1984fa5b15f13356f91db7c53
andrii.hreshchik <andrii.hreshchik@gmail.com>
2018-04-21 18:54:45 +0300

    HW_05 homework 21.04 18:54

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,18 +16,19 @@ public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
 
     @Transactional
     @Modifying
-    @Query
-    void delete(Meal entity);
+    @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+    int delete(@Param("id") int id, @Param("userId") int userId);
 
 
-    @Override
-    Optional<Meal> findById(Integer id);
+    Optional<Meal> findByIdAndUserId(Integer id, Integer userID);
+
+    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC")
+    List<Meal> findAll(@Param("userId") int userId);
 
     @Override
     @Transactional
     Meal save(Meal meal);
 
-    @Override
-    List<Meal> findAll(Sort sort);
+    List<Meal> findByDateTimeBetweenAndUser_Id(LocalDateTime startDate, LocalDateTime endDate, Integer userId, Sort sort);
 
 }

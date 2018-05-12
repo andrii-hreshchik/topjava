4c4a268dbb350c7c2c98875b085b29e52e8638af
andrii.hreshchik <andrii.hreshchik@gmail.com>
2018-05-12 19:36:27 +0300

    HW_06 task 1.3 started

--- src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -1,13 +1,17 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Assume;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.validation.ConstraintViolationException;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static java.time.LocalDateTime.of;
 import static ru.javawebinar.topjava.MealTestData.*;
@@ -15,6 +19,9 @@ import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 public abstract class AbstractMealServiceTest extends AbstractServiceTest {
+
+    @Autowired
+    private Environment environment;
 
     @Autowired
     protected MealService service;
@@ -78,9 +85,15 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest {
 
     @Test
     public void testValidation() throws Exception {
+        Assume.assumeFalse(skipValidation());
         validateRootCause(() -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "  ", 300), USER_ID), ConstraintViolationException.class);
         validateRootCause(() -> service.create(new Meal(null, null, "Description", 300), USER_ID), ConstraintViolationException.class);
         validateRootCause(() -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Description", 9), USER_ID), ConstraintViolationException.class);
         validateRootCause(() -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Description", 5001), USER_ID), ConstraintViolationException.class);
+    }
+
+    private boolean skipValidation() {
+        ArrayList<String> environments = new ArrayList<>(Arrays.asList(environment.getActiveProfiles()));
+        return environments.contains("jdbc");
     }
 }
